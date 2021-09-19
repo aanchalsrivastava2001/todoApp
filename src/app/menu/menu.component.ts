ddacea7358e38b383774cf46732a8f4c37bf54e0
aanchalsrivastava2001 <91000895+aanchalsrivastava2001@users.noreply.github.com>
2021-09-19 11:48:34 +0530

    Add files via upload

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,1 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+    //ng  isUserLoggedIn : boolean =false;
+  constructor(public hardcodedAuthenticatioService: HardcodedAuthenticationService) {
+
+   }
+
+  ngOnInit(): void {
+
+    //this.isUserLoggedIn =this.hardcodedAuthenticatioService.isUserLoggedIn();
+  }
+
+
+}

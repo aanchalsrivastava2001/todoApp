4ad53e6214ddbfd8a4d51eee44413cb83a45e8f2
aanchalsrivastava2001 <91000895+aanchalsrivastava2001@users.noreply.github.com>
2021-09-19 11:37:17 +0530

    Add files via upload

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,1 +1,15 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.css']
+})
+export class ErrorComponent implements OnInit {
+
+  constructor() { }
+ errorMessage= 'An error occured';
+  ngOnInit(): void {
+  }
+
+}
